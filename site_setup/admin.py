0ea40b619bf8644797d1d0d47de197b4973dcb07
Matheus Andrade <mat.reis.andrade@live.com>
2026-04-04 18:10:13 -0400

    Criando e configurando o model site_setup

--- site_setup/admin.py
+++ site_setup/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from django.http import HttpRequest
+from site_setup.models import MenuLink, SiteSetup
 
 
 # Register your models here.
@@ -8,3 +9,15 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'id', 'text', 'url_or_path'
     list_display_links = 'id', 'text', 'url_or_path'
     search_fields = 'id', 'text', 'url_or_path'
+
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description'
+
+    # Esse método verifica se o usuário tem permissão para 
+    # adicionar itens no model 
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        # Tem que retornar True ou False
+        # Verifica se há algum objeto no model
+        return not SiteSetup.objects.exists()
