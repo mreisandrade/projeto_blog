0657be4c711b0c3be17ae5c6c1a77fb3d2bcf273
Matheus Andrade <mat.reis.andrade@live.com>
2026-04-04 18:26:25 -0400

    Configurando o inline na área administrativa do SiteSetup

--- site_setup/admin.py
+++ site_setup/admin.py
@@ -3,17 +3,25 @@ from django.http import HttpRequest
 from site_setup.models import MenuLink, SiteSetup
 
 
+# Mostrar os elementos de MenuLink em SiteSetup
+class MenuLinkInline(admin.TabularInline):
+    model = MenuLink
+    # Quantidade de campos extras que aparece
+    extra = 1
+
+
 # Register your models here.
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path'
-    list_display_links = 'id', 'text', 'url_or_path'
-    search_fields = 'id', 'text', 'url_or_path'
+# @admin.register(MenuLink)
+# class MenuLinkAdmin(admin.ModelAdmin):
+#     list_display = 'id', 'text', 'url_or_path'
+#     list_display_links = 'id', 'text', 'url_or_path'
+#     search_fields = 'id', 'text', 'url_or_path'
 
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     list_display = 'title', 'description'
+    inlines = MenuLinkInline,
 
     # Esse método verifica se o usuário tem permissão para 
     # adicionar itens no model 
--- site_setup/models.py
+++ site_setup/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-class MenuLink(models.Model):
-    class Meta:
-        verbose_name = 'Menu Link'
-        verbose_name_plural = 'Menu Links'
-
-    text = models.CharField(max_length=50)
-    # Permitir colocar caminhos, urls, id, etc
-    url_or_path = models.CharField(max_length=2048)
-    # Abri o url em uma outra aba ou não
-    new_tab = models.BooleanField(default=False)
-
-
 class SiteSetup(models.Model):
     class Meta:
         verbose_name = 'Setup'
@@ -31,3 +18,27 @@ class SiteSetup(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+# Create your models here.
+class MenuLink(models.Model):
+    class Meta:
+        verbose_name = 'Menu Link'
+        verbose_name_plural = 'Menu Links'
+
+    text = models.CharField(max_length=50)
+    # Permitir colocar caminhos, urls, id, etc
+    url_or_path = models.CharField(max_length=2048)
+    # Abri o url em uma outra aba ou não
+    new_tab = models.BooleanField(default=False)
+    site_setup = models.ForeignKey(
+        SiteSetup,
+        # O que será feito quando o site_setup for apagado
+        on_delete=models.CASCADE,
+        # Campo não é obrigatório 
+        blank=True,
+        # Campo poder estar vazio 
+        null=True,
+        # Valor padrão
+        default=None,
+    )
