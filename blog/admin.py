da7c1096258261e355835be7ed342c7861bbaafc
Matheus Andrade <mat.reis.andrade@live.com>
2026-04-12 19:54:52 -0400

    Criado o model Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from blog.models import Tag, Category, Page
+from blog.models import Tag, Category, Page, Post
 
 
 # Register your models here.
@@ -59,3 +59,47 @@ class PageyAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('title',),
     }
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Campos mostrados
+    list_display = 'id', 'title', 'is_published', 'created_by'
+    # Campos com link para abrir a edição dos dados
+    list_display_links = 'title',
+    # Campos usados nas pesqueisas
+    search_fields = 'id', 'title', 'slug', 'excerpt', 'content', 'created_by'
+    # Quantidade de dados por páginas
+    list_per_page = 50
+    # Lista de filtros
+    list_filter = 'category', 'is_published'
+    # O que vai ser editável na tabela
+    list_editable = 'is_published',
+    # Ordem apresentada (ordem decrescente por id)
+    ordering = '-id',
+    # Campos somente para leitura
+    readonly_fields = 'created_at', 'updated_at', 'updated_by', 'created_by'
+    # Completa automaticamente o campo, neste caso, o slug
+    # o nome da tag
+    prepopulated_fields = {
+        "slug": ('title',),
+    }
+
+    autocomplete_fields = 'tags', 'category'
+
+
+    # Sobreescrevendo o método save_model para atribuir os 
+    # campos updated_by e created_by
+    # request: requisição que está sendo feito
+    # obj: objeto que está sendo modificado
+    # form: formulário utilizado
+    # change: verdadeiro quando estiver alterando o objeto
+    def save_model(self, request, obj, form, change):
+        # Caso esteja apenas atualizando o post
+        if change:
+            obj.updated_by =  request.user
+        else:
+            obj.created_by =  request.user
+
+        # Salva o objeto
+        obj.save()
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+# Usuário padrão do Django
+from django.contrib.auth.models import User
 
 from utils.rands import slugify_new
 
@@ -94,6 +96,12 @@ class Category(models.Model):
 # Páginas do blog (segue a estrutura do blog, mas com o conteúdo
 # em HTML provido a parte)
 class Page(models.Model):
+    # Configurações do model
+    class Meta:
+        verbose_name = 'Page'
+        verbose_name_plural = 'Pages'
+
+    # Título da página
     title = models.CharField(max_length=64)
     # Texto que vai representar a página na URL (parecido com id)
     slug = models.SlugField(
@@ -121,6 +129,143 @@ class Page(models.Model):
     def __str__(self) -> str:
         return self.title
 
+
+    # Sobreescrevendo o método que salva os dados
+    # Assim, pode-se fazer algo antes ou depois de salvar os dados
+    def save(self, *args, **kwargs):
+        # Se não existir uma slug
+        if not self.slug:
+            # Cria a slug
+            self.slug = slugify_new(self.title)
+
+        # É necessário chamar o super do método
+        # Para salvar os dados
+        super().save(*args, **kwargs)
+
+
+# Posts do blog 
+class Post(models.Model):
+    # Configurações do model
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    # Título do post
+    title = models.CharField(max_length=64)
+    # Texto que vai representar a página na URL (parecido com id)
+    slug = models.SlugField(
+        # Atribuindo com único
+        unique=True,
+        # Valor padrão
+        default=None,
+        # Aceita valores nulos
+        null=True,
+        # Aceita não definir valores
+        blank=True,
+        # Tamanho máximo 
+        max_length=255,
+    )
+    # Resumo do post
+    excerpt = models.CharField(max_length=150)
+    # Torna o post público apenas se for verdadeiro
+    is_published = models.BooleanField(
+        # Valor padrão
+        default=False,
+        # Texto de ajuda
+        help_text='Este campo precisará estar marcado para a página ser exibida publicamente'
+    )
+    # Conteúdo do post (HTML)
+    content = models.TextField()
+    # Capa do posts
+    cover = models.ImageField(
+        # Onde será armazenado o arquivo
+        # Na pasta ./media/post/<ano_atual>/<mês_atual>/
+        upload_to='posts/%Y/%m/',
+        # Aceita não definir valores
+        blank=True,
+        # Valor padrão
+        default='',
+    )
+    # Atribui a exibição da imagem da capa também dentro do
+    # conteúdo do post, ou não
+    cover_in_post_content = models.BooleanField(
+        # Valor padrão
+        default=True,
+        # Texto de ajuda
+        help_text='Exibir a imagem da capa também dentro do conteúdo do post',
+    )
+    # Quando o post foi criado
+    created_at = models.DateTimeField(
+        # Adiciona a data atual quando o post for CRIADO
+        auto_now_add=True,
+    )
+    # Quem criou o post
+    created_by = models.ForeignKey(
+        # model relacionado
+        User,
+        # O que fazer ao deletar o elemento da User
+        on_delete=models.SET_NULL,
+        # Aceita valores nulos
+        null=True,
+        # Aceita não definir valores
+        blank=True,
+        # Nome relacionado 
+        # Pode ser acessado por user.post.title
+        # Ou, da forma invertida, user.post_set.all(),
+        # que, neste caso, fica user.page_created_by.all()
+        related_name='page_created_by',
+    )
+    # Quando o post foi atualizado
+    updated_at = models.DateTimeField(
+        # Adiciona a data atual quando o post for ATUALIZADO
+        auto_now=True,
+    )
+    # Quem atualizou o post
+    updated_by = models.ForeignKey(
+        # model relacionado
+        User,
+        # O que fazer ao deletar o elemento da User
+        on_delete=models.SET_NULL,
+        # Aceita valores nulos
+        null=True,
+        # Aceita não definir valores
+        blank=True,
+        # Nome relacionado 
+        # Pode ser acessado por user.post.title
+        # Ou, da forma invertida, user.post_set.all(),
+        # que, neste caso, fica user.page_updated_by.all()
+        related_name='page_updated_by',
+    )
+    # Categoria do post
+    # Relação de muitos para um
+    category = models.ForeignKey(   
+        # model relacionado
+        Category,
+        # O que fazer ao deletar o elemento da Category
+        on_delete=models.SET_NULL,
+        # Valor padrão
+        default=None,
+        # Aceita valores nulos
+        null=True,
+        # Aceita não definir valores
+        blank=True,
+    )
+    # Tag do post
+    # Relação de muitos para muitos
+    tags = models.ManyToManyField(
+        # model relacionado
+        Tag,
+        # Valor padrão
+        default='',
+        # Aceita não definir valores
+        blank=True,
+    )
+
+
+    # Nome que é exibido ao chamar o model
+    def __str__(self) -> str:
+        return self.title
+    
 
     # Sobreescrevendo o método que salva os dados
     # Assim, pode-se fazer algo antes ou depois de salvar os dados
