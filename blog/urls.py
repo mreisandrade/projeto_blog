d88f7b704ae7b7a4035d6a1411b02fc76f1c8488
Matheus Andrade <mat.reis.andrade@live.com>
2026-04-16 18:37:14 -0400

    Iniciada a configuração da página do post

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from blog.views import index, post, page
+from blog.views import index, post, page, created_by, category
 
 
 # Namespace
@@ -11,5 +11,7 @@ urlpatterns = [
     path('', index, name='index'),
     # Usando o slug para acessar o post
     path('post/<slug:slug>/', post, name='post'),
-    path('page/', page, name='page'),
+    path('page/<slug:slug>/', page, name='page'),
+    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('category/<slug:slug>/', category, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,57 @@ def index(request):
     )
 
 
-def page(request):
+def created_by(request, author_pk):
+    # Posts
+    # Os parênteses são usados apenas para quebrar a linha
+    posts = (
+        Post
+        .objects
+        .get_published() # type: ignore
+        # Os __ significa que está pegando o campo pk do created_by
+        # (foreign key), neste caso
+        .filter(created_by__pk=author_pk)
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def category(request, slug):
+    # Posts
+    # Os parênteses são usados apenas para quebrar a linha
+    # Os __ significa que está pegando o campo slug da category
+    # (foreign key), neste caso
+    posts = (
+        Post
+        .objects
+        .get_published() # type: ignore
+        .filter(category__slug=slug)
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
@@ -41,11 +91,14 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+    # Pegando o post
+    post = Post.objects.get_published().filter(slug=slug).first() # type: ignore
+
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post,
         }
     )
