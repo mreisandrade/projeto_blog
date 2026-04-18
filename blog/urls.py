e34ca49f0eeeac4c3523c03c2adbe397fce311be
Matheus Andrade <mat.reis.andrade@live.com>
2026-04-18 14:41:45 -0400

    Finalizado a atualização das views para Classes Based Views para as outras views do app Blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 # from blog.views import index, post, page, created_by, category, tag, search
-from blog.views import PostListView, post, page, CreatedByListView, category, tag, search
-
+from blog.views import (
+    PostListView, PostDetailView, PageDetailView, 
+    CreatedByListView, CategoryListView, TagListView, 
+    SearchListView,
+) 
+    
 
 # Namespace
 app_name = 'blog'
@@ -12,20 +16,37 @@ urlpatterns = [
     # Functions Based Views
     # path('', index, name='index'),
     # Usando o slug para acessar o post
-    path('post/<slug:slug>/', post, name='post'),
-    path('page/<slug:slug>/', page, name='page'),
+    # path('post/<slug:slug>/', post, name='post'),
+    # path('page/<slug:slug>/', page, name='page'),
     # path('created_by/<int:author_pk>/', created_by, name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),
-    path('tag/<slug:slug>/', tag, name='tag'),
-    path('search/', search, name='search'),
+    # path('category/<slug:slug>/', category, name='category'),
+    # path('tag/<slug:slug>/', tag, name='tag'),
+    # path('search/', search, name='search'),
 
     # Classes Based Views
     # É necessário chamar o método as_view() pois o path espera
     # receber um callable
     path('', PostListView.as_view(), name='index'),
     path(
+        'post/<slug:slug>/', 
+        PostDetailView.as_view(), 
+        name='post'
+    ),
+    path(
+        'page/<slug:slug>/', 
+        PageDetailView.as_view(), 
+        name='page',
+    ),
+    path(
         'created_by/<int:author_pk>/', 
         CreatedByListView.as_view(), 
-        name='created_by'
+        name='created_by',
     ),
+    path(
+        'category/<slug:slug>/', 
+        CategoryListView.as_view(), 
+        name='category',
+    ),
+    path('tag/<slug:slug>/', TagListView.as_view(), name='tag'),
+    path('search/', SearchListView.as_view(), name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,14 @@
 from typing import Any
 
-from django.core.paginator import Paginator
-from django.db.models.query import QuerySet
+# from django.core.paginator import Paginator
+# from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 # Usado para condicionais com OU no Django
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.http import Http404, HttpRequest, HttpResponse
 # Para usar Classes Based Views no Django
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post, Page
 
@@ -73,6 +73,7 @@ class PostListView(ListView):
     template_name = 'blog/pages/index.html'
     # Definindo o nome do contexto enviado para o template
     # Variável dentro do context
+    # Onde serão buscado os dados no template
     context_object_name = 'posts'
     
     # A paginação já está pronta no Class Based View
@@ -211,84 +212,193 @@ class CreatedByListView(PostListView):
     # Para filtrar apenas os posts que estão publicados
     def get_queryset(self):
         queryset =  super().get_queryset()
-        queryset = queryset.filter(created_by__pk=self._temp_context['user'].pk)
+        queryset = queryset.filter(
+            created_by__pk=self._temp_context['user'].pk,
+        )
         return queryset
 
 
-def category(request, slug):
-    # Posts
-    # Os parênteses são usados apenas para quebrar a linha
-    # Os __ significa que está pegando o campo slug da category
-    # (foreign key), neste caso
-    posts = (
-        Post
-        .objects
-        .get_published() # type: ignore
-        .filter(category__slug=slug)
-    )
+# def category(request, slug):
+#     # Posts
+#     # Os parênteses são usados apenas para quebrar a linha
+#     # Os __ significa que está pegando o campo slug da category
+#     # (foreign key), neste caso
+#     posts = (
+#         Post
+#         .objects
+#         .get_published() # type: ignore
+#         .filter(category__slug=slug)
+#     )
 
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
 
-    if len(page_obj) == 0:
-        raise Http404()
+#     if len(page_obj) == 0:
+#         raise Http404()
 
-    page_title = f'{page_obj[0].category.name} - Categoria - '
+#     page_title = f'{page_obj[0].category.name} - Categoria - '
 
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
-            'page_title': page_title,
-        },
-    )
-
-
-def tag(request, slug):
-    # Posts
-    # Os parênteses são usados apenas para quebrar a linha
-    # Os __ significa que está pegando o campo slug da tag
-    # (foreign key), neste caso
-    posts = (
-        Post
-        .objects
-        .get_published() # type: ignore
-        .filter(tags__slug=slug)
-    )
-
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    if len(page_obj) == 0:
-        raise Http404()
-
-    page_title = f'{page_obj[0].tags.first().name} - Tag - '
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
-            'page_title': page_title,
-        }
-    )
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': page_title,
+#         },
+#     )
 
 
-def search(request):
-    search_value = request.GET.get('search', '').strip()
+# Class Based View equivalente a função "category"
+class CategoryListView(PostListView):
+    # Apresenta um erro caso não haja objetos a serem exibidos
+    allow_empty = False
 
-    # Posts
-    # Os parênteses são usados apenas para quebrar a linha
-    # Os __ significa que está pegando o campo slug da tag
-    # (foreign key), neste caso
-    posts = (
-        Post
-        .objects
-        .get_published() # type: ignore
-        .filter(
+    # Sobrescrevendo o método que opera a QuerySet
+    # Para filtrar apenas os posts que estão publicados
+    def get_queryset(self):
+        queryset =  super().get_queryset()
+        queryset = queryset.filter(
+            category__slug=self.kwargs.get('slug'),
+        )
+        return queryset
+    
+
+    # Sobrescrevendo o método que opera o contexto
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        page_title = f'{self.object_list[0].category.name} - Categoria - ' # type: ignore
+
+        # Atualizando o contexto com o título da página
+        context.update(
+            {
+                'page_title': page_title,
+            }
+        )
+
+        return context
+
+
+# def tag(request, slug):
+#     # Posts
+#     # Os parênteses são usados apenas para quebrar a linha
+#     # Os __ significa que está pegando o campo slug da tag
+#     # (foreign key), neste caso
+#     posts = (
+#         Post
+#         .objects
+#         .get_published() # type: ignore
+#         .filter(tags__slug=slug)
+#     )
+
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     if len(page_obj) == 0:
+#         raise Http404()
+
+#     page_title = f'{page_obj[0].tags.first().name} - Tag - '
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': page_title,
+#         }
+#     )
+
+
+# Class Based View equivalente a função "tag"
+class TagListView(PostListView):
+    # Apresenta um erro caso não haja objetos a serem exibidos
+    allow_empty = False
+
+    # Sobrescrevendo o método que opera a QuerySet
+    # Para filtrar apenas os posts que estão publicados
+    def get_queryset(self):
+        queryset =  super().get_queryset()
+        queryset = queryset.filter(
+            tags__slug=self.kwargs.get('slug'),
+        )
+        return queryset
+    
+
+    # Sobrescrevendo o método que opera o contexto
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        page_title = f'{self.object_list[0].tags.first().name} - Tag - ' # type: ignore
+
+        # Atualizando o contexto com o título da página
+        context.update(
+            {
+                'page_title': page_title,
+            }
+        )
+
+        return context
+
+
+# def search(request):
+#     search_value = request.GET.get('search', '').strip()
+
+#     # Posts
+#     # Os parênteses são usados apenas para quebrar a linha
+#     # Os __ significa que está pegando o campo slug da tag
+#     # (foreign key), neste caso
+#     posts = (
+#         Post
+#         .objects
+#         .get_published() # type: ignore
+#         .filter(
+#             # Dessa forma, seria o AND
+#             # title__icontains=search_value
+#             # Título contém search_value OU
+#             # O Q possibilita condicionais com o OU (|)
+#             Q(title__icontains=search_value) |
+#             # Excerto contém search_value OU
+#             Q(excerpt__icontains=search_value) |
+#             # Conteúdo contém search_value
+#             Q(content__icontains=search_value)
+#         )[0:PER_PAGE] # Pega apenas 9 valores
+#     )
+
+#     # Pega, no máximo, 30 caracteres
+#     page_title = f'{search_value[:30]} - Search - '
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': posts,
+#             'search_value': search_value,
+#             'page_title': page_title,
+#         },
+#     )
+
+
+# Class Based View equivalente a função "search"
+class SearchListView(PostListView):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        
+
+    # Sobrescrevendo o método realiza as configurações iniais
+    def setup(self, request, *args, **kwargs):
+        # Neste momento, o self.request ainda não está disponível
+        self._search_value = request.GET.get('search', '').strip()
+        return super().setup(request, *args, **kwargs)
+    
+
+    # Sobrescrevendo o método que opera a QuerySet
+    # Para filtrar os posts com base no campo de pesquisa
+    def get_queryset(self):
+        search_value = self._search_value
+
+        return super().get_queryset().filter(
             # Dessa forma, seria o AND
             # title__icontains=search_value
             # Título contém search_value OU
@@ -299,60 +409,161 @@ def search(request):
             # Conteúdo contém search_value
             Q(content__icontains=search_value)
         )[0:PER_PAGE] # Pega apenas 9 valores
-    )
+    
 
-    # Pega, no máximo, 30 caracteres
-    page_title = f'{search_value[:30]} - Search - '
+    # Sobrescrevendo o método get 
+    # O método get deve retornar um HTTP response
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if self._search_value.strip() == '':
+            return redirect('blog:index')
+        
+        return super().get(request, *args, **kwargs)
+    
 
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': posts,
-            'search_value': search_value,
-            'page_title': page_title,
-        },
-    )
+    # Sobrescrevendo o método que opera o contexto
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
+        search_value = self._search_value
+        page_title = f'{search_value[:30]} - Search - '
 
-def page(request, slug):
-    # Pegando o post
-    page_object = (
-        Page.objects
-        .filter(is_published=True)
-        .filter(slug=slug)
-        .first() 
-    ) # type: ignore
+        # Atualizando o contexto com o título da página
+        context.update(
+            {
+                'page_title': page_title,
+                'search_value': search_value,
+            }
+        )
 
-    if page_object is None:
-        raise Http404()
-
-    page_title = f'{page_object.title} - Página - '
-
-    return render(
-        request,
-        'blog/pages/page.html',
-        {
-            'page': page_object,
-            'page_title': page_title,
-        },
-    )
+        return context
 
 
-def post(request, slug):
-    # Pegando o post
-    post_object = Post.objects.get_published().filter(slug=slug).first() # type: ignore
+# def page(request, slug):
+#     # Pegando o post
+#     page_object = (
+#         Page.objects
+#         .filter(is_published=True)
+#         .filter(slug=slug)
+#         .first() 
+#     ) # type: ignore
 
-    if post_object is None:
-        raise Http404()
+#     if page_object is None:
+#         raise Http404()
 
-    page_title = f'{post_object.title} - Post - '
+#     page_title = f'{page_object.title} - Página - '
 
-    return render(
-        request,
-        'blog/pages/post.html',
-        {
-            'post': post_object,
-            'page_title': page_title,
-        },
-    )
+#     return render(
+#         request,
+#         'blog/pages/page.html',
+#         {
+#             'page': page_object,
+#             'page_title': page_title,
+#         },
+#     )
+
+
+# Class Based View equivalente a função "page"
+class PageDetailView(DetailView):
+    # É necessário informar o model que está sendo usado
+    model = Page
+
+    # Por padrão, o Django tenta carregar um template padrão
+    # Por tanto, é necessário alterar o template
+    # Dica: Use seus próprios nomes, não os padrões do Django,
+    # neste caso
+    template_name = 'blog/pages/page.html'
+    # Definindo o nome do contexto enviado para o template
+    # Variável dentro do context
+    # Onde serão buscado os dados no template
+    context_object_name = 'page'
+    # Campo do slug
+    slug_field = 'slug'
+
+
+    # Sobrescrevendo o método que opera o contexto
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Obtendo a dado mostrado na DetailView
+        page_object = self.get_object() 
+        page_title = f'{page_object.title} - Página - ' # type: ignore
+
+        # Atualizando o contexto com o título da página
+        context.update(
+            {
+                'page_title': page_title,
+            }
+        )
+
+        return context
+    
+
+    # Sobrescrevendo o método que opera a QuerySet
+    # Para filtrar apenas os posts que estão publicados
+    def get_queryset(self):
+        return super().get_queryset().filter(
+           is_published=True, 
+        )
+
+
+# def post(request, slug):
+#     # Pegando o post
+#     post_object = Post.objects.get_published().filter(slug=slug).first() # type: ignore
+
+#     if post_object is None:
+#         raise Http404()
+
+#     page_title = f'{post_object.title} - Post - '
+
+#     return render(
+#         request,
+#         'blog/pages/post.html',
+#         {
+#             'post': post_object,
+#             'page_title': page_title,
+#         },
+#     )
+
+
+# Class Based View equivalente a função "post"
+class PostDetailView(DetailView):
+    # É necessário informar o model que está sendo usado
+    model = Post
+
+    # Por padrão, o Django tenta carregar um template padrão
+    # Por tanto, é necessário alterar o template
+    # Dica: Use seus próprios nomes, não os padrões do Django,
+    # neste caso
+    template_name = 'blog/pages/post.html'
+    # Definindo o nome do contexto enviado para o template
+    # Variável dentro do context
+    # Onde serão buscado os dados no template
+    context_object_name = 'post'
+    # Campo do slug
+    slug_field = 'slug'
+
+
+    # Sobrescrevendo o método que opera o contexto
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Obtendo a dado mostrado na DetailView
+        post_object = self.get_object() 
+        post_title = f'{post_object.title} - Página - ' # type: ignore
+
+        # Atualizando o contexto com o título da página
+        context.update(
+            {
+                'page_title': post_title,
+            }
+        )
+
+        return context
+    
+
+    # Sobrescrevendo o método que opera a QuerySet
+    # Para filtrar apenas os posts que estão publicados
+    def get_queryset(self):
+        return super().get_queryset().filter(
+           is_published=True, 
+        )
